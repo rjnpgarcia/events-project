24d7240c2bd4c17900d9108aa86404a2cb682703
rjnpgarcia <102405417+rjnpgarcia@users.noreply.github.com>
2022-12-21 03:20:40 +0800

    walk in events button and spacing

--- src/components/assets/WalkIn.jsx
+++ src/components/assets/WalkIn.jsx
@@ -1,55 +1,109 @@
+// React
+import React from "react";
+// CSS and Icons
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import classes from "./WalkIn.module.css";
+import "./WalkInSlider.module.css";
+// React-Bootstrap
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+// React Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Grid, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/navigation";
 
-function WalkIn() {
+function WalkIn(props) {
   return (
     <Container className={classes.containerSlider}>
-      <Row className={classes.row1}>
-        <Col xs={10} className={classes.column}>
-          <ul>
-            <li className={classes.title}>My Seminar</li>
-            <li className={classes.address}>
-              Prominencs Health Plan Office 1155 South Congress Avenue
-            </li>
-            <li className={classes.date}>
-              <CalendarMonthIcon className={classes.calendarIcon} /> Sat,
-              October 8, 2022, 10:00am
-            </li>
-          </ul>
-        </Col>
-        <Col xs={2}>
-          <p className={classes.day}>
-            Oct
-            <br />8
-          </p>
-        </Col>
-      </Row>
-      <div className={classes.borderLine}>
-        <hr className={classes.line} />
+      <Swiper
+        slidesPerView={1}
+        grid={{ rows: 2 }}
+        spaceBetween={0}
+        navigation={{
+          prevEl: ".button-prev-slide",
+          nextEl: ".button-next-slide",
+        }}
+        modules={[Grid, Navigation]}
+        className="mySwiper"
+      >
+        {props.contents.map((content) => {
+          if (content.walkIn === true && content.id % 2 === 0) {
+            return (
+              <SwiperSlide className={classes.row2}>
+                <Row>
+                  <Col xs={10} className={classes.column}>
+                    <ul>
+                      <li className={classes.title} key={content.id}>
+                        {content.title}
+                      </li>
+                      <li className={classes.address}>{content.address}</li>
+                      <li className={classes.date}>
+                        <CalendarMonthIcon className={classes.calendarIcon} />{" "}
+                        {content.when}
+                      </li>
+                    </ul>
+                  </Col>
+                  <Col xs={2}>
+                    <p className={classes.day}>
+                      Oct
+                      <br />
+                      {content.day}
+                    </p>
+                  </Col>
+                </Row>
+              </SwiperSlide>
+            );
+          }
+          if (content.walkIn === true) {
+            return (
+              <SwiperSlide className={classes.row1}>
+                <Row>
+                  <Col xs={10} className={classes.column}>
+                    <ul>
+                      <li className={classes.title} key={content.id}>
+                        {content.title}
+                      </li>
+                      <li className={classes.address}>{content.address}</li>
+                      <li className={classes.date}>
+                        <CalendarMonthIcon className={classes.calendarIcon} />{" "}
+                        {content.when}
+                      </li>
+                    </ul>
+                  </Col>
+                  <Col xs={2}>
+                    <p className={classes.day}>
+                      Oct
+                      <br />
+                      {content.day}
+                    </p>
+                  </Col>
+                </Row>
+              </SwiperSlide>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </Swiper>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 d-flex justify-content-end">
+            <div className="button-prev-slide">
+              <button>left</button>
+              <button>prev</button>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="button-next-slide">
+              <button>right</button>
+              <button>next</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <Row className={classes.row2}>
-        <Col xs={10} className={classes.column}>
-          <ul>
-            <li className={classes.title}>My Seminar</li>
-            <li className={classes.address}>
-              Prominencs Health Plan Office 1155 South Congress Avenue
-            </li>
-            <li className={classes.date}>
-              <CalendarMonthIcon className={classes.calendarIcon} /> Sat,
-              October 8, 2022, 10:00am
-            </li>
-          </ul>
-        </Col>
-        <Col xs={2}>
-          <p className={classes.day}>
-            Oct
-            <br />8
-          </p>
-        </Col>
-      </Row>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "swiper/css/bundle";
 import "./index.css";
 import App from "./App";
+// import "swiper/css";
+// import "swiper/css/grid";
+// import "swiper/css/pagination";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
